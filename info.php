58346cc48e41be14a19c66cdde6da9119584c5a7
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-08 10:37:10 +0200

    files header.php and footer.php added

---
+++ info.php
@@ -0,0 +1,32 @@
+
+<?php
+
+//ASSOCIATIVE ARRAY HEADER ET FOOTER
+$infosite = array(
+  //Infos header
+  'titre' => 'Ça me botte!',
+  'soustitre' => 'Toujours droit dans ses bottes...',
+  //Infos footer
+  'auteur' => 'Jean-Louis Dupond',
+  'annee' =>  '2017',
+  'pays' => 'France');
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a> -->
