918a58d47a45503fe3fc4c098aa2b115bb2c6319
FlorentLequibain <florent.lequibain@gmail.com>
2017-09-11 10:04:01 +0200

    restart project

--- index.php
+++ index.php
@@ -1,97 +1,30 @@
-<?php include ("info.php"); ?>
+<?php include("info.php"); ?>
 
 
 
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="icon" href="favicon.ico" />
-        <!-- Place favicon.ico in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+ <?php
 
-<!-- ~~~~~~~~~~~~~~~ HEADER ~~~~~~~~~~~~~~~ -->
-<h1> <?php echo $infosite ['titre']; ?> </h1>
-<h2> <?php echo $infosite ['soustitre']; ?></h2>
-<!-- ~~~~~~~~~~~~~~~ HEADER ~~~~~~~~~~~~~~~ -->
+foreach ($articles as $element2) {
 
 
+?>
 
-<!-- ~~~~~~~~~~~~~~~ ARTICLES ~~~~~~~~~~~~~~~ -->
-<div class="container">
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure class="figureimg"><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
-  <article class="article">
-    <h4>blabla + prix</h4>
-    <figure><img src="img/botte1.jpg" alt=""></figure>
-    <a href="#">Voir l'article</a>
-  </article>
-
+<div class="card">
+  <img class="card-img-top" src="<?php echo $element2['image'] ;?>" alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title"><?php echo $element2['titre'] ;?></h4>
+    <p class="card-text">
+      <?php echo $element2['description']; ?>
+    </p>
+    <p><?php echo $element2['prix'] ;?></p>
+    <a href="#" class="btn btn-primary">Voir l'article</a>
+  </div>
 </div>
-<!-- ~~~~~~~~~~~~~~~ ARTICLES ~~~~~~~~~~~~~~~ -->
 
 
-<!-- ~~~~~~~~~~~~~~~ FOOTER ~~~~~~~~~~~~~~~ -->
-<!-- ~~~~~~~~~~~~~~~ FOOTER ~~~~~~~~~~~~~~~ -->
+<?php
 
+}
 
-
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-        <script src="js/bootstrap.js"></script>
-    </body>
-</html>
+?>
---
+++ info.php
@@ -0,0 +1,50 @@
+<?php
+
+$articles = [
+
+    'article1' => [
+
+      'titre' => 'boots en daim',
+      'prix' => '120$',
+      'image' => 'img/botte1.jpg',
+      'description' => 'super confortable'],
+
+      'article2' => [
+
+        'titre' => 'boots en croco',
+        'prix' => '100$',
+        'image' => 'img/botte2.jpg',
+        'description' => 'super beau'],
+
+        'article3' => [
+
+          'titre' => 'boots en vachette',
+          'prix' => '140$',
+          'image' => 'img/botte3.jpg',
+          'description' => 'super odeur'],
+
+          'article4' => [
+
+            'titre' => 'boots en agneau',
+            'prix' => '90$',
+            'image' => 'img/botte4.jpg',
+            'description' => 'super mignion'],
+
+            'article5' => [
+
+              'titre' => 'boots en cuir',
+              'prix' => '139$',
+              'image' => 'img/botte5.jpg',
+              'description' => 'super noir'],
+
+              'article6' => [
+
+                'titre' => 'boots en mamouth',
+                'prix' => '80$',
+                'image' => 'img/botte6.jpg',
+                'description' => 'super préhistorique'],
+
+    ];
+
+
+?>
