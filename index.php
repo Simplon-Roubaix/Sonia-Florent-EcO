028088241ba0b969bbe2bde27be2795fa9864a86
FlorentLequibain <florent.lequibain@gmail.com>
2017-09-11 15:34:06 +0200

    add hover on articles

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <?php include("header.php"); ?>
 
+<div class="container">
 
 
  <?php
@@ -12,20 +13,23 @@ foreach ($articles as $element2) {
 ?>
 
 <div class="card">
-  <img class="card-img-top" src="<?php echo $element2['image'] ;?>" alt="Card image cap">
+
+  <img class="card-img-top cardimg" src="<?php echo $element2['image'] ;?>" alt="Card image cap">
   <div class="card-block">
-    <h4 class="card-title"><?php echo $element2['titre'] ;?></h4>
-    <p class="card-text">
-      <?php echo $element2['description']; ?>
-    </p>
-    <p><?php echo $element2['prix'] ;?></p>
-    <a href="ficheproduit.php?
+    <h4 class="card-title"><?php echo $element2['titre'];?> - <?php echo $element2['description']; ?></h4>
+    <p class="prix"><?php echo $element2['prix'] ;?></p>
+  </div>
+
+  <div class="hoverclass">
+    <a class="buylink"
+       href="ficheproduit.php?
              titre=<?php echo $element2['titre']; ?>&amp;
              prix=<?php echo $element2['prix']; ?>&amp;
              image=<?php echo $element2['image']; ?>&amp;
              description=<?php echo $element2['description']; ?>
                 " class="btn btn-primary">Voir l'article</a>
   </div>
+
 </div>
 
 
@@ -35,5 +39,6 @@ foreach ($articles as $element2) {
 
 ?>
 
+</div>
 
 <?php include("footer.php"); ?>
