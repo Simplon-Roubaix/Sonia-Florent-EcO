2eb9dfd4b8c0d7c2c59bb995ec422f7890df2905
FlorentLequibain <florent.lequibain@gmail.com>
2017-09-12 09:30:32 +0200

    add ficheproduit css

--- ficheproduit.php
+++ ficheproduit.php
@@ -4,12 +4,20 @@
 
 
 
-<p>
-  <?php echo $_GET['titre'] ;?>
-  <?php echo $_GET['prix'] ;?>
-  <?php echo $_GET['image'] ;?>
-  <?php echo $_GET['description'] ;?>
-</p>
+
+<div class="containerficheproduit">
+  <img class="imageproduit" src="<?php echo $_GET['image'] ;?>" alt="article en vente">
+
+  <div class="infosficheproduit">
+    <h3 class="titreficheproduit"><?php echo $_GET['titre'] ;?></h3>
+    <p class="descriptionficheproduit"><?php echo $_GET['description'] ;?></p>
+    <p class="prixficheproduit"><?php echo $_GET['prix'] ;?></p>
+  </div>
+</div>
+
+
+
+
 
 
 
